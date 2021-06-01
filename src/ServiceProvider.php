233a308f841208337bc349382438ad46767392ce
Rolin Bos <rolinbos1@icloud.com>
2021-06-01 13:59:55 +0200

    Init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Raynl\GenerateSlug;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    //
+}
---
+++ src/Slug.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Raynl\GenerateSlug;
+
+use Statamic\Entries\Entry;
+
+class Slug
+{
+    /**
+     * Generate a unique slug
+     * it checks if the slug exists on the collection
+     * if not then return the slug
+     *
+     * @param string $collection
+     * @param $slug
+     * @param string $seperator
+     * @param int $start_number
+     *
+     * @return string
+     */
+    public static function generateSlug(string $collection, $slug, $seperator = '-', $start_number = 1): string
+    {
+        $slug = \Str::slug($slug, '-');
+
+        if (Entry::query()->where('collection', $collection)->where('slug', $slug)->first())
+        {
+            $slug .= $seperator.'1';
+            $number = $start_number;
+            while (Entry::query()->where('collection', $collection)->where('slug', $slug)->first()) {
+                $explode = explode($seperator, $slug);
+
+                $last = array_key_last($explode);
+                $explode[$last] = (string)$number;
+
+                $slug = implode('-', $explode);
+
+                $number = (int)end($explode) + 1;
+            }
+        }
+
+        return $slug;
+    }
+}
